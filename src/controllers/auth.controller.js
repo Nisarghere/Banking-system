e87ae5d8d1760b9fcb0ec7e06a1372ea30c2772c
Nisarghere <nisarg72020@gmail.com>
2026-07-20 15:39:58 +0530

    Email verification

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,77 @@
 const userModel = require("../models/users.model");
+const jwt = require("jsonwebtoken");
 
-
-exports.registerUser=(req, res)=>{
+exports.registerUser=async (req, res)=>{
     const {name, email, password }= req.body
-    
+
+    const isExist = await userModel.findOne({
+        email: email
+    })
+
+    if (isExist){
+        return res.status(422).json({
+            message:"User already exist !",
+            status:"failed"
+        })
+    }
+
+    const user = await userModel.create({
+        email, 
+        password, 
+        name
+    })
+
+    const token = jwt.sign(
+        {userId:user._id},
+         process.env.JWT_SECRET,
+         {expiresIn:"3d"})
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        user:{
+            id:user._id,
+            name:user.name,
+            email:user.email
+        }
+    })
+
+}
+
+exports.loginUser = async (req, res)=>{
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({
+        email:email
+    }).select("+password")
+
+    if (!user){
+        return res.status(401).json({
+            message:"Invalid credentials!"
+        })
+    }
+    const isvalidPasswd = await user.comparePassword(password)
+
+    if (!isvalidPasswd){
+        return res.status(401).json({
+            message:"Invalid credentials"
+        })
+    }
+
+    const token = jwt.sign({userId:user._id},
+        process.env.JWT_SECRET,
+        {expiresIn:"3d"}
+    )
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        id:user._id,
+        email:user.email,
+        name:user.name
+    })
+
+     
+
+
 }
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,5 +1,5 @@
 const  mongoose  = require("mongoose");
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -28,19 +28,20 @@ const userSchema = new mongoose.Schema({
         timestamps:true
     })
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
     if (!this.isModified("password")) {
-        return next();
+        return ;
     }
 
     this.password = await bcrypt.hash(this.password, 10);
-
-    next();
-});
+    return;
+ });
 
 userSchema.methods.comparePassword = async function (password){
     return bcrypt.compare(password, this.password)
 }
+
+
 
 const userModel = mongoose.model("user", userSchema)
 
